1462ed8ec066056b1396d9c8989c73443dd937d9
Aleksandr <sh7aleksandr@gmail.com>
2022-12-05 05:11:26 +0200

    edit yml for bd save

--- html/index_v1.php
+++ html/index_v1.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+
 $user = "root";
 $password = "secret";
 $database = "autoparc";
@@ -9,15 +10,18 @@ $table_cars = "cars";
 
 try {
     $db1 = new PDO("mysql:host=db;dbname=$database", $user, $password);
-    echo "<h2>$table</h2>";
+    echo "<h2>parks</h2><ol>";
     foreach ($db1->query("SELECT * FROM $table") as $row) {
-        echo $row['id'] .' '. $row['serial_number'] . ' ' . $row['address'] . "<br/>";
+//        echo "<li>" . $row['id'] . ' ' . $row['serial_number'] . ' ' . $row['address'] . "</li>";
+        echo "<h4>" . $row['id'] .' '. $row['serial_number'] . ' ' . $row['address'] . "</h4>";
     }
-    echo "<h2>$table_cars</h2>";
+    echo "<h2>cars</h2>";
     foreach ($db1->query("SELECT * FROM $table_cars") as $row) {
-        echo $row['id'] .' '. $row['park_id'] . ' ' . $row['model'] . ' ' . $row['year'] .  ' ' . $row['price'] . "<br/>";
+        echo "<h4>" . $row['id'] .' '. $row['park_id'] . ' ' . $row['model'] . ' ' . $row['year'] .  ' ' . $row['price'] . "</h4>";
     }
+    echo "</ol>";
 } catch (PDOException $e) {
     d($e::class, $e->getFile(), $e->getLine(), $e->getMessage());
+//    print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
