799c0433f5806f4e17bcaad2ac18ab1dff226e35
Aleksandr <sh7aleksandr@gmail.com>
2022-12-21 07:57:11 +0200

    test2

--- html/public/index.php
+++ html/public/index.php
@@ -1,63 +1,22 @@
 <?php
 
-class Room
-{
-    public static $location = "wwwww";
+use Config\Config;
 
-    public static function seyWel1()
-    {
-        echo 'stati method';
-    }
+require_once dirname(__DIR__) . '/Config/constants.php';
+require_once BASE_DIR . '/vendor/autoload.php';
+
+$dotenv = \Dotenv\Dotenv::createUnsafeImmutable(BASE_DIR);
+$dotenv->load();
+
+try {
+
+    $pdo = new PDO(
+        'mysql:host=db;dbname=taxi',
+        Config::get('db.user'),
+        Config::get('db.password')
+    );
+
+    d($pdo);
+} catch (PDOException $exception) {
+    d('Exception', $exception->getMessage());
 }
-
-echo Room::$location;
-$myRoom=new Room();
-echo $myRoom::seyWel1();
-
-
-
-
-//class Room
-//{
-//    private $color='111';
-////    public $color='111';
-//
-//    public function getColor()
-//    {
-//        echo $this->color;
-////        return $this->color;
-//    }
-//
-//    public function setColor(string $color)
-//    {
-//        $this->color = $color;
-//    }
-//
-//}
-//
-//$obj=new Room();
-////$obj->color='222';
-////$obj->getColor();
-//$obj->setColor('533355');
-//$obj->getColor();
-
-//use Config\Config;
-//
-//require_once dirname(__DIR__) . '/Config/constants.php';
-//require_once BASE_DIR . '/vendor/autoload.php';
-//
-//$dotenv = \Dotenv\Dotenv::createUnsafeImmutable(BASE_DIR);
-//$dotenv->load();
-//
-//try {
-//
-//    $pdo = new PDO(
-//        'mysql:host=db;dbname=taxi',
-//        Config::get('db.user'),
-//        Config::get('db.password')
-//    );
-//
-//    d($pdo);
-//} catch (PDOException $exception) {
-//    d('Exception', $exception->getMessage());
-//}
