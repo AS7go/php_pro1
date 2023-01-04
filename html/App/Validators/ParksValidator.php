61191e2f38d1fad7c921ec1ab816826768208bc8
Aleksandr <sh7aleksandr@gmail.com>
2023-01-05 00:03:17 +0200

    hw19

--- html/App/Validators/ParksValidator.php
+++ html/App/Validators/ParksValidator.php
@@ -9,17 +9,18 @@ use App\Validators\Base\BaseValidator;
 class ParksValidator extends BaseValidator
 {
     protected array $rules = [
-        'serial_number' => '/[a-zA-Z0-9_]{3,}/',
+        'serial_number' => '/^[a-zA-Z0-9_]{3,16}$/',
         'address' => '/[a-zA-Z0-9_]{3,}/',
     ];
 
     protected array $errors = [
-        'serial_number' => 'Serial number should contain only "a-z A-Z 0-9 _" and length should be more than 3 symbols',
-        'address' => 'Address should contain only "a-z A-Z 0-9 _" and length should be more than 3 symbols',
-    ];
+
 
 //    public function checkIfExists(string $serialNumber): bool|Park
 //    {
 //        return Park::findBy('serial_number', $serialNumber);
 //    }
+        'serial_number' => 'Serial number should contain only "a-z A-Z 0-9 _" and length should be more than 3 symbols and less than 16',
+        'address' => 'Address should contain only "a-z A-Z 0-9 _" and length should be more than 3 symbols',
+    ];
 }
--- html/routes/web.php
+++ html/routes/web.php
@@ -115,6 +115,6 @@ $router->add(
     [
         'controller' => \App\Controllers\Admin\ParksController::class,
         'action' => 'destroy',
-        'method' => 'POST'
+        'method' => 'GET'
     ]
 );
