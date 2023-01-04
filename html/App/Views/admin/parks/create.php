56cb729a37747caf9bf9e806fc12fdf1791b459e
Aleksandr <sh7aleksandr@gmail.com>
2023-01-04 19:27:34 +0200

    hw19

---
+++ html/App/Views/admin/parks/create.php
@@ -0,0 +1,36 @@
+<?php
+\Core\View::render('admin/blocks/header', ['pageTitle' => 'Create park']);
+$errors = $errors ?? [];
+?>
+    <div class="container pt-5 pb-5">
+        <div class="row">
+            <h3>Create Park</h3>
+        </div>
+        <div class="row"
+            <div class="col-1"></div>
+            <div class="col-10">
+                <form class="card mt-5" method="POST" action="<?= url('admin/parks/store') ?>">
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="serial_number" class="form-label">Serial Number</label>
+                            <input type="text" class="form-control" id="serial_number" name="serial_number">
+                        </div>
+                        <div class="mb-3">
+                            <label for="address" class="form-label">Address</label>
+                            <input type="text" class="form-control" id="address" name="address">
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="mb-3 form-check">
+                            <button type="submit" class="btn btn-primary">Create</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+<?php
+\Core\View::render('admin/blocks/footer');
