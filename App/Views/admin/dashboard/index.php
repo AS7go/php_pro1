f5ddac4f2f502dba0ea70680a66b1629cd744767
Aleksandr <sh7aleksandr@gmail.com>
2023-01-04 23:29:24 +0200

    hw19

--- App/Views/admin/dashboard/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-\Core\View::render('admin/blocks/header', ['pageTitle' => 'Dashboard']);
-
-\Core\View::render('admin/blocks/footer');
--- App/Views/home/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Core\View::render('blocks/header', ['pageTitle' => 'Home']);
-
-echo '<h3>Home page</h3>';
-
-\Core\View::render('blocks/footer');
